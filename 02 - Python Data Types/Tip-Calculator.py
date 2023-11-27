ed81f7061fc1ca40b321212e6ef61ae3b3374b0b
romiandriansah <romiandriansah08@gmail.com>
2023-11-28 04:59:29 +0800

    initial commit

--- 02 - Python Data Types/Tip-Calculator.py
+++ 02 - Python Data Types/Tip-Calculator.py
@@ -1,4 +1,9 @@
 print("Welcome to the tip calculator.")
-bill = input("How much the total bill? $")
-tip = input("How much tip would you like to give? 5, 10, 15?")
-split = input("How many people to split the bill?")
+bill = float(input("How much the total bill? $"))
+tip = int(input("How much tip would you like to give? 5, 10, 15?"))
+split = int(input("How many people to split the bill?"))
+tip_percent = tip/100
+tip_amount = bill * tip_percent
+total_bill = bill + tip_amount
+total_split = total_bill/split
+# total_final = round()
