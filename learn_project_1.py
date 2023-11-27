bdeadcfd89883fd46f1975c55d33275c822909f5
romiandriansah <romiandriansah08@gmail.com>
2023-11-27 23:48:41 +0800

    string and variable project

---
+++ learn_project_1.py
@@ -0,0 +1,4 @@
+print("""This is the first day I learn about string and variable
+on python, I learn how to create project use string and variable
+from UDEMY Course with Dr. Angela Yu as instructor""")
+print("Bootcamp 100 Days Python")
