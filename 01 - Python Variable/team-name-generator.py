486e6771a52f56bc32a25320fd41b8cb994428b5
romiandriansah <romiandriansah08@gmail.com>
2023-11-28 03:14:08 +0800

    initial commit

--- 01 - Python Variable/team-name-generator.py
+++ 01 - Python Variable/team-name-generator.py
@@ -1,4 +1,5 @@
-print("Welcome to Football Team Name Generator.")  # greating
+print("Welcome to Football Team Name Generator.")  #
+g
 colour = input("What your colour? \n")
 animal = input("What is your favorite animal? \n")
 print("Your football team name could be " + colour + " " + animal)
