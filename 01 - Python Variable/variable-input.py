2236262ca6fe4127ed73da1af7090ca3ba66aebf
romiandriansah <romiandriansah08@gmail.com>
2023-12-01 22:25:34 +0800

    initial commit

---
+++ 01 - Python Variable/variable-input.py
@@ -0,0 +1,8 @@
+print(""" This project I created to remember me
+my journey of learning how to code in Python.
+Because I am very interesting to learn about programming.
+My motivation is I want to get a better job as programming.""")
+print("Bootcamp 100 Days Python")
+
+name = input("What's your name?\n")  # input function
+print(" Hello, " + name)
