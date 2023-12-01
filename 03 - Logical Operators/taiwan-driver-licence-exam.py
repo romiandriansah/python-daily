20d19f2356acc82adb140d416d04909692a31524
romiandriansah <romiandriansah08@gmail.com>
2023-12-02 00:34:07 +0800

    initial commit

--- 03 - Logical Operators/taiwan-driver-licence-exam.py
+++ 03 - Logical Operators/taiwan-driver-licence-exam.py
@@ -3,15 +3,20 @@ print("Hello, Welcome to registration of "
 
 ask1 = input("Do you own Taiwanese scooter license"
              " (regular motorcycle 50 – 250cc) for at least 1 year? Y/N \n")
-ask2 = input("Do you over 20 years old? Y/N \n")
-ask2 = input("If you are foreigner, an ARC that has been issued"
-             " for at least 6 months or longer. Y/N \n")
 
-if ask1 == "Y" or "y":
-    print("Congratulations")
-elif ask2 == "Y" or "y":
-    print("Congratulations")
-elif ask3== "Y" or "y":
-    print("Congratulations")
+
+if ask1 == "y":
+    ask2 = input("Do you over 20 years old? Y/N \n")
+    if ask2 == "y":
+        ask3 = input("If you are foreigner, an ARC that has been issued"
+                     " for at least 6 months or longer. Y/N \n")
+        if ask3 == "y":
+            print("Congratulation! You have met the qualifications")
+            print("You can registration this program")
+        else:
+            print("Sorry! You must met 3 of qualification above")
+    else:
+        print("Sorry! You must met 3 of qualification above")
 else:
     print("You can't yet to take this program")
+    print("Your scooter license must at least 1 year")
