81cd2cbec2d48888717dae7154f37938f80a8273
romiandriansah <romiandriansah08@gmail.com>
2023-12-01 23:32:16 +0800

    initial commit

---
+++ 03 - Logical Operators/taiwan-driver-licence-exam.py
@@ -0,0 +1,17 @@
+print("Hello, Welcome to registration of "
+      "Heavy Motorcycle License Program")
+
+ask1 = input("Do you own Taiwanese scooter license"
+             " (regular motorcycle 50 – 250cc) for at least 1 year? Y/N \n")
+ask2 = input("Do you over 20 years old? Y/N \n")
+ask2 = input("If you are foreigner, an ARC that has been issued"
+             " for at least 6 months or longer. Y/N \n")
+
+if ask1 == "Y" or "y":
+    print("Congratulations")
+elif ask2 == "Y" or "y":
+    print("Congratulations")
+elif ask3== "Y" or "y":
+    print("Congratulations")
+else:
+    print("You can't yet to take this program")
